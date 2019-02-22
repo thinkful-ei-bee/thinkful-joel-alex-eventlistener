e0474cf3d424aa7dd845e0971b62a54ec68908bb
Joel Corey <joelcorey@fastmail.com>
2019-02-22 09:46:12 -0800

    Fix jQuery

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function clicker(){
-  
-  
-  $('.thumbnails').on('click', 'img', function(event){
-    let imageSource1 = $(this).attr('src');
-    let imageAlt1 = $(this).attr('alt');
+'use strict';
+
+function clicker(){  
+  $('.thumbnail').on('click', function(event){
+    let imageSource1 = $(this).find('img').attr('src');
+    let imageAlt1 = $(this).find('img').attr('alt');
     $('.hero img').attr('src', imageSource1);
     $('.hero img').attr('alt', imageAlt1);
     
-  })
+  });
+
 
   
 }
